5f62461e27b4e253da735bf6507811849bb0a4d9
jack nguyen <jack.nguyen@helius-software.com>
2021-11-20 09:10:50 +0700

    setup: config for pwa

---
+++ pages/_offline.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import Head from 'next/head';
+
+const Offline: FC = () => {
+  return (
+    <>
+      <Head>
+        <title>next-pwa example</title>
+      </Head>
+      <div>Offline</div>
+    </>
+  );
+};
+
+export default Offline;
